249b1487635254e25c5e58c7acc41a1622a00cd8
Chandrashekar <chandrashekar31.v@gmail.com>
2024-05-29 13:25:04 +0530

    MongoDB and express setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 //require('dotenv').config()
+import { app } from "./app.js"
 import ConnectDB from "./db/index.js"
 
 ConnectDB()
+.then(()=>{
+    app.listen(3000,()=>{
+        console.log("server started on 3000")
+    })
+})
