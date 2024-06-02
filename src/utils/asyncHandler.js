1bc3d9ec2523264dee4e5313f723246ca40db220
Chandrashekar <chandrashekar31.v@gmail.com>
2024-06-02 20:05:46 +0530

    async code handler was create

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,11 @@
 
-const asyncHandler = (fn) => async(req,res,next)=>{
-    try{
-        await fn(req,res,next)
-    }catch(err){
-        res.status(err.code || 5000).json({
-            success:false,
-            message:err.message
+const asyncHandler = (fn) => {
+    return (req, res, next) => {
+        Promise.resolve(fn(req,res,next)).catch((err)=>{
+            next(err)
         })
     }
 }
 
 
-export {asyncHandler}
+export { asyncHandler }
