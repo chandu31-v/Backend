dab6398037b9540296e789d3062b1bf3924f65a5
Chandrashekar <chandrashekar31.v@gmail.com>
2024-06-02 20:02:03 +0530

    mongo db connection was established

--- src/db/index.js
+++ src/db/index.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 async function ConnectDB() {
 
     try {
-        const mongoConnect = await mongoose.connect(`mongodb+srv://dracarysdragonfire8:${process.env.DB_PASSWORD}@backend.cssseva.mongodb.net/?retryWrites=true&w=majority&appName=backend`)
-        console.log(mongoConnect)
+        const mongoConnect = await mongoose.connect(`mongodb+srv://dracarysdragonfire8:${process.env.DB_PASSWORD}@cluster0.q8jpq2f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+        //console.log(mongoConnect)
+        console.log("DB connected")
     } catch (err) {
-        console.log(err)
+        console.log("DB connection error")
     }
-
 }
+
 export default ConnectDB
